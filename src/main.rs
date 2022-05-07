6289b5f4f9ff583dd3c915bca9e2aa2cb42be24d
TheJPMZ <monzon.jpmz@gmail.com>
2022-05-07 16:28:51 -0600

    ➕ Added the main menu

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the calculator!");
+
+    loop {
+        println!("[1] Celcius to Fahrenheit");
+        println!("[2] Fahrenheit to Celcius");
+        println!("[3] Quit");
+
+        let mut choice = String::new();
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read line");
+
+        let choice: u8 = match choice.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match choice {
+            1 => { 
+                println!("===Celcius===");
+            },
+            2 => { 
+                println!("===Fahrenheit===");
+            },
+            3 => { break; },
+            _ => { continue; },
+        }
+    }
 }
